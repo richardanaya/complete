92d2cd25beaedba9ac01e1965cb53b0f1d96f9f4
richard <richard>
2019-11-10 20:52:30 -0800

    adding block until

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,16 @@ use core::sync::atomic::{AtomicBool, Ordering};
 static IS_COMPLETE: AtomicBool = AtomicBool::new(false);
 
 #[inline(always)]
-pub fn complete() {
+pub fn mark_complete() {
     IS_COMPLETE.store(true, Ordering::Release);
 }
 
 #[inline(always)]
 pub fn is_complete() -> bool {
     IS_COMPLETE.load(Ordering::Acquire)
+}
+
+#[inline(always)]
+pub fn block_until_complete() {
+    while !is_complete() {}
 }
